7ee852d5f99264198b423da9b7ecb64c5016783b
Vũ Quang Hải <aperture147@gmail.com>
2022-06-11 01:28:17 +0700

    add option for logging

--- app.py
+++ app.py
@@ -9,21 +9,25 @@ parser.add_argument("-a", "--artboard-only",
                     default=False,
                     action="store_true",
                     help="Split artboards in Illustrator into separated SVG files")
-parser.add_argument("-c", "--no-cleanup",
+parser.add_argument("--no-cleanup",
                     default=False,
                     action="store_true",
                     help="Don't cleanup temporary file")
 parser.add_argument("-p", "--inkscape-path",
-                    default='inkscape',
+                    default=None,
                     help="Inkscape execution file path")
+parser.add_argument("-l", "--log",
+                    default=False,
+                    action="store_true",
+                    help="Write log to file")
+
 args = parser.parse_args()
 
-DEFAULT_WINDOWS_INKSCAPE_PATH = "C:/Program Files/Inkscape/bin/inkscape.com"
-
-extractor = Extractor(inkscape_path=DEFAULT_WINDOWS_INKSCAPE_PATH)
+extractor = Extractor(inkscape_path=args.inkscape_path)
 
 extraction_id = extractor.extract(
     'test.pdf',
+    inkscape_log=args.log,
     artboard_only=args.artboard_only,
     cleanup=not args.no_cleanup)
 
