3d1b8c47ff774623de6c6f2f2e3d854eca1367d9
Vũ Quang Hải <aperture147@gmail.com>
2022-06-11 01:04:03 +0700

    add  help

--- app.py
+++ app.py
@@ -5,16 +5,26 @@ from extractor import Extractor
 
 parser = ArgumentParser()
 
-parser.add_argument("-a", "--artboard-only", default=False, action="store_true")
-parser.add_argument("-c", "--not-cleanup", default=False, action="store_true")
-parser.add_argument("-p", "--inkscape-path", default='inkscape')
+parser.add_argument("-a", "--artboard-only", 
+                    default=False,
+                    action="store_true",
+                    help="Split artboards in Illustrator into separated SVG files")
+parser.add_argument("-c", "--no-cleanup",
+                    default=False,
+                    action="store_true",
+                    help="Don't cleanup temporary file")
+parser.add_argument("-p", "--inkscape-path",
+                    default='inkscape',
+                    help="Inkscape execution file path")
 args = parser.parse_args()
 
-extractor = Extractor("C:/Program Files/Inkscape/bin/inkscape")
+DEFAULT_WINDOWS_INKSCAPE_PATH = "C:/Program Files/Inkscape/bin/inkscape.com"
+
+extractor = Extractor(inkscape_path=DEFAULT_WINDOWS_INKSCAPE_PATH)
 
 extraction_id = extractor.extract(
     'test.pdf',
     artboard_only=args.artboard_only,
-    cleanup=not args.not_cleanup)
-    
+    cleanup=not args.no_cleanup)
+
 print(f'extract ID: {extraction_id}')
