7db2bb142d73d7e5008e5baf24f0d57ca525f0ff
Vũ Quang Hải <aperture147@gmail.com>
2022-06-11 00:53:24 +0700

    objectify Extractor

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from extractor import extract
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from extractor import Extractor
 
-extraction_id = extract('test.pdf', artboard_only=True)
+
+parser = ArgumentParser()
+
+parser.add_argument("-a", "--artboard-only", default=False, action="store_true")
+parser.add_argument("-c", "--not-cleanup", default=False, action="store_true")
+parser.add_argument("-p", "--inkscape-path", default='inkscape')
+args = parser.parse_args()
+
+extractor = Extractor("C:/Program Files/Inkscape/bin/inkscape")
+
+extraction_id = extractor.extract(
+    'test.pdf',
+    artboard_only=args.artboard_only,
+    cleanup=not args.not_cleanup)
+    
 print(f'extract ID: {extraction_id}')
